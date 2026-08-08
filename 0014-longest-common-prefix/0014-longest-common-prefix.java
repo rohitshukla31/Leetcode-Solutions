a7832a25ad8ca08c048c6378ba78af39110f3a7e
Rohit Shukla <rohitshuklabbu31@gmail.com>
2026-08-08 23:30:29 +0530

    Time: 0 ms (100%), Space: 43.3 MB (32.74%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        
+        String pre = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+
+            while (!strs[i].startsWith(pre)) {
+                pre = pre.substring(0, pre.length() - 1);
+
+                if (pre.length() == 0) {
+                    return "";
+                }
+            }
+        }
+
+        return pre;
+    }
+}
