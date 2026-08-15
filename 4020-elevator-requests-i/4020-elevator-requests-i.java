69dd88b9bda61157c719c3722881b5c404a2efa8
Rohit Shukla <rohitshuklabbu31@gmail.com>
2026-08-15 21:51:56 +0530

    Time: 1 ms (98.33%), Space: 44.9 MB (45.32%) - LeetHub

--- 4020-elevator-requests-i/4020-elevator-requests-i.java
+++ 4020-elevator-requests-i/4020-elevator-requests-i.java
@@ -2,7 +2,7 @@ class Solution {
     public int elevatorRequests(int n, int[] requests) {
         int time=requests[0];
         for(int i=1;i<requests.length;i++){
-            time +=Math.abs(requests[i]-requests[i-1]);
+            time +=Math.abs(requests[i-1]-requests[i]);
         }
         return time;
     }
