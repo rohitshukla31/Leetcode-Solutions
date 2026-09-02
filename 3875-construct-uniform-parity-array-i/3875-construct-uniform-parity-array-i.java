a37470d57a8cd93c5a348b65692dab38e616aa7d
Rohit Shukla <rohitshuklabbu31@gmail.com>
2026-09-02 10:18:12 +0530

    Time: 0 ms (100%), Space: 44.8 MB (97.05%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+    // int n=nums1.length;
+    // int new[]=new int[n];
+    // new [0]=nums1[0];
+    // for(int i=1;i<n;i++){
+    //     if(new[0]%2==0){
+    //         new[i]=
+    //     }
+    // }
+        return true;
+    }
+}
