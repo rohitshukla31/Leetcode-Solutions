00bad7b7397f7563b5e7d233c1f68406643fae84
Rohit Shukla <rohitshuklabbu31@gmail.com>
2026-09-09 22:58:41 +0530

    Time: 3 ms (99.09%), Space: 44 MB (91.57%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,18 +1,56 @@
 class Solution {
     public String reverseWords(String s) {
 
-        String[] word = s.split("\\s+");
+        // String[] word = s.split("\\s+");
 
-        int right=word.length-1;
-        StringBuilder sb=new StringBuilder();
+        // int right=word.length-1;
+        // StringBuilder sb=new StringBuilder();
 
-        while(right >= 0){
+        // while(right >= 0){
 
-            sb.append(word[right]).append(" ");
-            right--;
+        //     sb.append(word[right]).append(" ");
+        //     right--;
             
-        }
+        // }
 
-        return sb.toString().trim();
+        // return sb.toString().trim();
+
+        //==========optimal approach=========
+        StringBuilder ans = new StringBuilder();
+        int i = s.length()-1;
+
+        while(i >= 0){
+            // remove all trailing spaces
+            while(i>0 && s.charAt(i) == ' '){
+                i--;
+            }
+
+            //cheack value of i
+            if(i < 0){
+                break;
+            }
+
+            int j = i;
+            // find the starting index of word
+            while(j >= 0 && s.charAt(j) != ' '){
+                j--;
+            }
+            // jaise hi j space wali index pe aayega toh rukk jayega
+            //ab iss word ko apne ans me append kr do
+            ans.append(s.substring(j+1, i+1));
+            //remove extra space where j is standing and add space in ans
+            while(j >= 0 && s.charAt(j) == ' '){
+                j--;
+            }
+
+            // j < 0 no space needed
+            // j > 0 space needed
+            if(j >= 0){
+                ans.append(' ');
+            }
+            //place i at last index of remaining string
+            i = j;
+        }
+        return ans.toString();
     }
 }
